c7732c253005c479923dd6d236ecf25242406d07
memirozdemir <emremr1123@gmail.com>
2024-12-12 21:08:33 +0300

    Add project files.

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            CategoryValidator validator = new CategoryValidator();
+            ValidationResult results = validator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var x in results.Errors)
+                {
+                    ModelState.AddModelError(x.PropertyName, x.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
